db06fd5eae3cfebd9fdabed9c8e10499ff4aa179
AngelF4 <angel.hdz.g@icloud.com>
2025-10-04 12:05:50 -0600

    Initial Commit

---
+++ HackNasa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackNasa
+//
+//  Created by Angel Hernández Gámez on 04/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackNasa/HackNasaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackNasaApp.swift
+//  HackNasa
+//
+//  Created by Angel Hernández Gámez on 04/10/25.
+//
+
+import SwiftUI
+
+@main
+struct HackNasaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
